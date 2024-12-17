5395c6722e7afa2180f10dbc39fac13eee801e49
madlyn03 <hongnmp@gmail.com>
2024-12-17 21:34:20 +0700

    update code

--- src/API/axiosClient.jsx
+++ src/API/axiosClient.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 // http://localhost:8000
-const env = "dev";
+const env = "prod";
 const baseURL =
   env === "dev" ? "http://localhost:8000" : "https://cosmetics-be.onrender.com";
 const axiosClient = axios.create({
